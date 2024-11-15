5763e3a6b70000ef8ac7387dcfd13c22e0853ce4
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2024-11-15 04:34:24 -0500

    s

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, users, me } = require('../controllers/auth'); // Check the path
+const { signup, login, users, me } = require('../routers/auth'); // Check the path
 const verifyToken = require('../middleware/auth');
 
 // Define routes
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/users', users);
-router.get('/me',  me);
+router.get('/me', verifyToken, me);
 console.log({ signup, login, users, me }); // Ensure all functions are not undefined
 
 module.exports = router;
