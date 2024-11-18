395121eeee8004bee68b10061fb2611c87d2bd37
B CHUCHEL ASHISH <85539405+ashishrockz@users.noreply.github.com>
2024-11-18 06:01:32 -0500

    gre

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ const fs = require('fs');
 const path = require('path');
 
 // Ensure the uploads folder exists
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir);
-}
+// const uploadsDir = path.join(__dirname, 'uploads');
+// if (!fs.existsSync(uploadsDir)) {
+//   fs.mkdirSync(uploadsDir);
+// }
 
 // Define the storage configuration for multer
 const storage = multer.diskStorage({
